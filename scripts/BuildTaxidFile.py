7d65fe08275ffa85227836117591637fe2c67ccb
Andres Graterol <graterol.andres0@knights.ucf.edu>
2023-03-26 15:45:04 -0400

    Initial file commit for BuildTaxidFile

---
+++ scripts/BuildTaxidFile.py
@@ -0,0 +1,32 @@
+# Andres Graterol 
+# CAP 6545
+# tRNA thermometer project
+# ================================================
+# Builds the taxid and species corresponding  
+# 
+# ================================================
+import pandas as pd
+
+def process_file(input_file, output_file):
+    file = pd.read_csv(input_file, header=None)
+
+
+    return 
+
+def main():
+
+    # Try using the input files associated with the RNA first
+    # These are the files associated with the regression model
+    # Archaea File first 
+    input_file1 = ''
+    # Bacteria Model second
+    input_file2 = ''
+
+    '''Analyze the separate files before we combine them into one file'''
+    # Output file 1
+    output_file1 = ''
+    output_file2 = ''
+
+    return 
+
+main()
